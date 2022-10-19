f91baf6b108f6d6d185381df049fb8c7b9ead25f
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-19 16:40:22 -0400

    "commit"
    ear
    
    clear
    clear
    "commit"

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import express from 'express'
-import pg from 'pg'
-import { creds } from './creds.js'
+import { getAllCustomers,getCustomerById } from './src/customers.js'
 
-const { Pool } = pg 
+
 
 const app = express()
 
 
 
-app.get('/customers',async (req,res) => {
-    const pool = new Pool(creds)
-    const customers = await pool.query ("SELECT * FROM customers")
-    .catch(err => res.status(500).send(err))
-    res.send(customers.rows)
-    pool.end()
-})
-
+app.get('/customers',getAllCustomers)
+app.get('/customers/:customerId', getCustomerById)
 app.listen(3030, () => console.log ('Listening on http://localhost:3030...'))
 
